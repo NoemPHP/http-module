6e9f9aacb3bc1e7b1694352c40dd33d24937ef74
Biont <Biont@users.noreply.github.com>
2021-08-30 17:37:15 +0000

    Apply automatic changes

--- src/ResponseEmitter.php
+++ src/ResponseEmitter.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-
 namespace Noem\Http;
-
 
 use Psr\Http\Message\ResponseInterface;
 
@@ -18,12 +16,9 @@ class ResponseEmitter
 
 // Step 1: Send the "status line".
         $statusLine = sprintf(
-            'HTTP/%s %s %s'
-            ,
-            $response->getProtocolVersion()
-            ,
-            $response->getStatusCode()
-            ,
+            'HTTP/%s %s %s',
+            $response->getProtocolVersion(),
+            $response->getStatusCode(),
             $response->getReasonPhrase()
         );
         header($statusLine, true); /* The header replaces a previous similar header. */
@@ -31,10 +26,8 @@ class ResponseEmitter
 // Step 2: Send the response headers from the headers list.
         foreach ($response->getHeaders() as $name => $values) {
             $responseHeader = sprintf(
-                '%s: %s'
-                ,
-                $name
-                ,
+                '%s: %s',
+                $name,
                 $response->getHeaderLine($name)
             );
             header($responseHeader, false); /* The header doesn't replace a previous similar header. */
--- src/RouteLoader.php
+++ src/RouteLoader.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Noem\Http;
 
-
 use FastRoute\RouteCollector;
 use Invoker\InvokerInterface;
 use Noem\Http\Attribute\Route;
